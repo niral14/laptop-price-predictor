522f8e9b618c5da190059be5a593e1f266408dc3
Niral Botale <92090518+niral14@users.noreply.github.com>
2025-02-28 01:24:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,23 +12,119 @@ with gzip.open('df.pkl.gz', 'rb') as f:
 
 st.title("Laptop Price Predictor")
 
-# User Inputs
-company = st.selectbox('Brand', df['Company'].unique())
-type = st.selectbox('Type', df['TypeName'].unique())
-ram = st.selectbox('RAM (in GB)', [2, 4, 6, 8, 12, 16, 24, 32, 64])
-weight = st.number_input('Weight of the Laptop')
-touchscreen = st.selectbox('Touchscreen', ['No', 'Yes'])
-ips = st.selectbox('IPS', ['No', 'Yes'])
-screen_size = st.slider('Screen size (in inches)', 10.0, 18.0, 13.0)
+# Function to pre-fill inputs based on button click
+def set_configuration(config):
+    st.session_state['company'] = config['company']
+    st.session_state['type'] = config['type']
+    st.session_state['ram'] = config['ram']
+    st.session_state['weight'] = config['weight']
+    st.session_state['touchscreen'] = config['touchscreen']
+    st.session_state['ips'] = config['ips']
+    st.session_state['screen_size'] = config['screen_size']
+    st.session_state['resolution'] = config['resolution']
+    st.session_state['cpu'] = config['cpu']
+    st.session_state['hdd'] = config['hdd']
+    st.session_state['ssd'] = config['ssd']
+    st.session_state['gpu'] = config['gpu']
+    st.session_state['os'] = config['os']
+
+# Default values (predefined laptop configurations)
+configurations = {
+    "Windows i3": {
+        'company': 'Dell',
+        'type': 'Notebook',
+        'ram': 8,
+        'weight': 1.6,
+        'touchscreen': 'No',
+        'ips': 'Yes',
+        'screen_size': 14.0,
+        'resolution': '1920x1080',
+        'cpu': 'Intel Core i3',
+        'hdd': 0,
+        'ssd': 256,
+        'gpu': 'Intel',
+        'os': 'Windows'
+    },
+    "Windows i5": {
+        'company': 'HP',
+        'type': 'Ultrabook',
+        'ram': 8,
+        'weight': 1.5,
+        'touchscreen': 'No',
+        'ips': 'Yes',
+        'screen_size': 15.6,
+        'resolution': '1920x1080',
+        'cpu': 'Intel Core i5',
+        'hdd': 0,
+        'ssd': 512,
+        'gpu': 'Intel',
+        'os': 'Windows'
+    },
+    "Windows i7": {
+        'company': 'Lenovo',
+        'type': 'Ultrabook',
+        'ram': 16,
+        'weight': 1.8,
+        'touchscreen': 'No',
+        'ips': 'Yes',
+        'screen_size': 15.6,
+        'resolution': '2560x1440',
+        'cpu': 'Intel Core i7',
+        'hdd': 0,
+        'ssd': 1024,
+        'gpu': 'NVIDIA',
+        'os': 'Windows'
+    },
+    "MacBook Air": {
+        'company': 'Apple',
+        'type': 'Ultrabook',
+        'ram': 8,
+        'weight': 1.24,
+        'touchscreen': 'No',
+        'ips': 'Yes',
+        'screen_size': 13.3,
+        'resolution': '2560x1600',
+        'cpu': 'Apple M1',
+        'hdd': 0,
+        'ssd': 256,
+        'gpu': 'Apple',
+        'os': 'Mac'
+    }
+}
+
+# Buttons to set configurations
+col1, col2, col3, col4 = st.columns(4)
+with col1:
+    if st.button("Windows i3"):
+        set_configuration(configurations["Windows i3"])
+with col2:
+    if st.button("Windows i5"):
+        set_configuration(configurations["Windows i5"])
+with col3:
+    if st.button("Windows i7"):
+        set_configuration(configurations["Windows i7"])
+with col4:
+    if st.button("MacBook Air"):
+        set_configuration(configurations["MacBook Air"])
+
+# User Inputs (Pre-filled if button clicked)
+company = st.selectbox('Brand', df['Company'].unique(), key='company')
+type = st.selectbox('Type', df['TypeName'].unique(), key='type')
+ram = st.selectbox('RAM (in GB)', [2, 4, 6, 8, 12, 16, 24, 32, 64], key='ram')
+weight = st.number_input('Weight of the Laptop', key='weight')
+touchscreen = st.selectbox('Touchscreen', ['No', 'Yes'], key='touchscreen')
+ips = st.selectbox('IPS', ['No', 'Yes'], key='ips')
+screen_size = st.slider('Screen size (in inches)', 10.0, 18.0, 13.0, key='screen_size')
 resolution = st.selectbox('Screen Resolution', ['1920x1080', '1366x768', '1600x900',
                                                  '3840x2160', '3200x1800', '2880x1800',
-                                                 '2560x1600', '2560x1440', '2304x1440'])
-cpu = st.selectbox('CPU', df['Cpu brand'].unique())
-hdd = st.selectbox('HDD (in GB)', [0, 128, 256, 512, 1024, 2048])
-ssd = st.selectbox('SSD (in GB)', [0, 8, 128, 256, 512, 1024])
-gpu = st.selectbox('GPU', df['Gpu brand'].unique())
-os = st.selectbox('OS', df['os'].unique())
+                                                 '2560x1600', '2560x1440', '2304x1440'], key='resolution')
+cpu = st.selectbox('CPU', df['Cpu brand'].unique(), key='cpu')
+hdd = st.selectbox('HDD (in GB)', [0, 128, 256, 512, 1024, 2048], key='hdd')
+ssd = st.selectbox('SSD (in GB)', [0, 8, 128, 256, 512, 1024], key='ssd')
+gpu = st.selectbox('GPU', df['Gpu brand'].unique(), key='gpu')
+os = st.selectbox('OS', df['os'].unique(), key='os')
 
+# Predict Price Button
 if st.button('Predict Price'):
     # Convert touchscreen and IPS to numerical format
     touchscreen = 1 if touchscreen == 'Yes' else 0
@@ -38,11 +134,11 @@ if st.button('Predict Price'):
     X_res, Y_res = map(int, resolution.split('x'))
     ppi = ((X_res**2) + (Y_res**2))**0.5 / screen_size
 
-    # **Pass categorical variables as they are** (Let the model's preprocessor handle them)
+    # Prepare input array
     query = np.array([company, type, ram, weight, touchscreen, ips,
                       ppi, cpu, hdd, ssd, gpu, os], dtype=object).reshape(1, -1)
 
-    # **Ensure input format matches what the model was trained on**
-    predicted_price = np.exp(pipe.predict(query)[0])  # Apply exponential function if necessary
+    # Predict price
+    predicted_price = np.exp(pipe.predict(query)[0])
 
     st.title(f"The predicted price of this configuration is ₹{int(predicted_price):,}")
